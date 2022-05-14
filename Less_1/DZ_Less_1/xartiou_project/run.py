e13e805bbca31571fab775422d4688a4c749878b
xartiou <xartiou@gmail.com>
2022-05-14 08:47:19 +0300

    DZ_Less_1 работает но стили не подтягивает

---
+++ Less_1/DZ_Less_1/xartiou_project/run.py
@@ -0,0 +1,11 @@
+from wsgiref.simple_server import make_server
+
+from xartiou_framework.main import Framework
+from urls import routes, fronts
+
+
+application = Framework(routes, fronts)
+
+with make_server('', 8080, application) as httpd:
+    print("Запуск через порт 8080...")
+    httpd.serve_forever()
