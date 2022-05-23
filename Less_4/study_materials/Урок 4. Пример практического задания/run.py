afbca621f1cc81dfceceb5d6005bb5b14e656ecb
xartiou <xartiou@gmail.com>
2022-05-23 19:55:08 +0300

    DZ_Less_4 создающие паттерны, новые модели, новые пути.

---
+++ Less_4/study_materials/Урок 4. Пример практического задания/run.py
@@ -0,0 +1,9 @@
+from wsgiref.simple_server import make_server
+from simba_framework.main import Framework
+from urls import routes, fronts
+
+application = Framework(routes, fronts)
+
+with make_server('', 8080, application) as httpd:
+    print("Запуск на порту 8080...(http://127.0.0.1:8080/)")
+    httpd.serve_forever()
---
+++ Less_4/study_materials/Урок 4. Пример практического задания/urls.py
@@ -0,0 +1,26 @@
+from datetime import date
+from views import Index, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
+
+
+# front controller
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
+}
